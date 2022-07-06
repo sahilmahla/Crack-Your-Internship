f8f1deba593fd65785b65588a5dc6a08ce0fcde3
Sahil Mahla <74190139+sahilmahla@users.noreply.github.com>
2022-07-06 14:22:14 +0530

    Time: 3 ms (60.76%), Space: 6 MB (41.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0) return 0;
+        int a[n+1];
+      a[0] = 1; 
+        a[1] = 1;
+        
+        for(int i = 2;i<n;i++){
+            a[i] = a[i-1]+a[i-2];
+        }
+        
+        return a[n-1];
+    }
+};
